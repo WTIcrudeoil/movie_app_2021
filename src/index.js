f542f44293f7a7002ec2b8c1e0277f70cba95f22
WTIcrudeoil <ldh7928@gmail.com>
2021-04-18 17:01:38 +0900

    2.2 JSX and Dynamic component generation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Potato from './potato';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    
   </React.StrictMode>
   ,
   document.getElementById('root')
--- src/potato.js
+++
@@ -1,7 +0,0 @@
-function Potato(){
-    return (
-        <h3>I love potato</h3>
-    )
-}
-export default Potato;
-//How to make component in react
