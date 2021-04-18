1f6f73e121b920a2ee4dd5b439543234bdd6a8fd
WTIcrudeoil <ldh7928@gmail.com>
2021-04-18 16:27:14 +0900

    2.0 Understanding about component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import Potato from './potato';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
-
+//component라고 부름 , is function return html // JSX meaning
