0730a14368c178c0afd6dc32a9b47ec12434d069
WTIcrudeoil <ldh7928@gmail.com>
2021-04-19 21:39:27 +0900

    Rendering the movie data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import axios from "axios";
+import Movie from "./movie";
 
 class App extends React.Component{
   state = {
@@ -8,8 +9,10 @@ class App extends React.Component{
     movies:[]
   };
   getMovies = async() =>{
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    const {data:{data :{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
     // axios.get속도 느릴수 있음 우리는  js에게 기다려 달라고 해야함
+    console.log(movies);
+    this.setState({movies,isLoading:false});
   };
   componentDidMount(){
     this.getMovies();
@@ -17,8 +20,11 @@ class App extends React.Component{
   
 
   render(){
-    const {isLoading} = this.state
-    return <div>{isLoading ? "Loading" : "We are ready"}   
+    const {isLoading,movies} = this.state
+    return <div>{isLoading ? "Loading" : movies.map(movie =>{
+      console.log(movie);
+      return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+    })}   
     </div>
 
 
