19bc04718503c1ba00ad42e9125425a047cb6583
WTIcrudeoil <ldh7928@gmail.com>
2021-04-22 23:29:23 +0900

    someCssChanged

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,20 +1,31 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import "./movie.css";
 
 //state 필요없으면 굳이 class component 안써도됨
 
 function Movie({id,year,title,summary,poster,genres}){
-    return <div className="movie">
-            <img src={poster} alt={title} title={title} />
-            <div className="movie__date">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__genres">{year}</h5>
-        <ul className="movie__genres">{(genres.map((genre,index) => <li key={index} className="genres__genre">{genre}</li>))}</ul>
-        <p className="movie__summary">{summary.slice(0,150)}...</p>
-    </div>
-
-    </div>
+    return (
+        
+            <div className="movie">
+                <Link to={{
+            pathname:"/movie-detail",
+            state:{
+                year,title,summary,poster,genres
+                 }
+                     }}>
+                    <img src={poster} alt={title} title={title} />
+                    <div className="movie__date">
+                        <h3 className="movie__title">{title}</h3>
+                        <h5 className="movie__genres">{year}</h5>
+                        <ul className="movie__genres">{(genres.map((genre,index) => <li key={index} className="genres__genre">{genre}</li>))}</ul>
+                        <p className="movie__summary">{summary.slice(0,150)}...</p>
+                    </div>
+                </Link>
+            </div>
+        
+    )
 }
 
 Movie.propTypes = {
